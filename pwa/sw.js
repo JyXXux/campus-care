28264c4ce3a1b1d3d224b4cb55efb8642af4a0ed
JyXXux <jyotishkabhaduri18@gmail.com>
2026-09-18 23:21:36 +0530

    Add PWA redirect launcher, project docs, and gitignore

---
+++ pwa/sw.js
@@ -0,0 +1,53 @@
+const CACHE = "campus-care-shell-v3";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-mask-192.png",
+  "./icons/icon-mask-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(SHELL);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; })
+        .map(function (k) { return caches.delete(k); }));
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  const url = new URL(event.request.url);
+  if (event.request.method !== "GET") return;
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      const fetched = fetch(event.request).then(function (response) {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(function (cache) { cache.put(event.request, copy); });
+        }
+        return response;
+      }).catch(function () {
+        return cached;
+      });
+      return cached || fetched;
+    })
+  );
+});
